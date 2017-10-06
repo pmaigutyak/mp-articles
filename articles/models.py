239aa14d3df3aa8a90e9f8ac0a1f55ab6c266556
Paul Maigutyak <pmaigutyak@ettera.com>
2017-10-06 13:28:38 +0300

    Change ugettext --> ugettext_lazy

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse_lazy
 
 from ckeditor_uploader.fields import RichTextUploadingField
